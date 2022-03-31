bb00c996ebc7200a344e11d30e00e31f2d9e5976
tychusyuan <48231351+tychusyuan@users.noreply.github.com>
2022-03-31 21:05:44 +0800

    Create sqlforeach.py

---
+++ opshell/applylog/sqlforeach.py
@@ -0,0 +1,23 @@
+import pymysql.cursors
+
+# Connect to the database
+connection = pymysql.connect(host='localhost',
+                             user='user',
+                             password='passwd',
+                             database='db',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+with connection:
+    lst=[]
+    with connection.cursor() as cursor:
+        sql = "select `id` FROM `users` WHERE `email`=%s"
+        cursor.execute(sql, ('webmaster@python.org',))
+        result = cursor.fetchone()
+        lst.append(result["id"])
+
+    with connection.cursor() as cursor:
+        for item in lst:
+            sql = "delete from  `users` where id = %s)"
+            cursor.execute(sql, (item))
+            connection.commit()
+
