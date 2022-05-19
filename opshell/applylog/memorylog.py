d43edbfff07f12bae06137ee9636951b0076de7e
tychusyuan <48231351+tychusyuan@users.noreply.github.com>
2022-05-19 16:54:35 +0800

    Update memorylog.py

--- opshell/applylog/memorylog.py
+++ opshell/applylog/memorylog.py
@@ -1,22 +1,38 @@
+# -*- coding: utf-8 -*-
+
+import sys
 import gc
 from time import sleep
 from datetime import datetime
-import psutil
 
-LVE = 0.5
+LVE = 0.4
 BLOCKSIZE = 1024 * 1024
+
+def sysMem():
+    with open("/proc/meminfo") as meminfo:
+        for item in meminfo:
+            if item.startswith("MemTotal"):
+                total_mem = int(item.split()[1])
+            elif item.startswith("MemAvailable"):
+                aval_mem = int(item.split()[1])
+                break
+            else:
+                pass
+    return total_mem*1024,aval_mem*1024
 
 def plog(s):
     print("%s %s" % (datetime.now().strftime('%Y-%m-%d %H:%M:%S'),s))
 
 def memorySpace():
-    mem = psutil.virtual_memory()
-    plog(int((mem.available - (mem.total*LVE)) / BLOCKSIZE))
-    return int((mem.available - (mem.total*LVE)) / BLOCKSIZE)
+    total_mem , aval_mem = sysMem()
+    plog((total_mem - aval_mem)/float(total_mem))
+    return int((aval_mem  - (total_mem * (1-LVE) )) / BLOCKSIZE)
 
 def memoryUp(lst):
     while memorySpace() > 0:
-        lst.append(1 << 7864320)
+        lst.append(fillBlock())
+    
+        plog(len(lst))
 
 def memoryDown(lst):
     while len(lst) > 0:
@@ -25,13 +41,21 @@ def memoryDown(lst):
             gc.collect()
         else:
             break
+        plog(len(lst))
+
+def fillBlock():
+    lst=[]
+    for idx in range(0,8192):
+        lst.append("NcOSK1oobg9cqVKC_dCYV1y6WNhqzNEN")
+    lst.append(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    return "".join(lst)
 
 if __name__ == "__main__":
     lst=[]
     while True:
         for idx in range(0,20):
             memoryDown(lst)
-            if idx == 0:
+            if idx == 1:
                 memoryUp(lst)
 
-            sleep(3)
+            sleep(1)
