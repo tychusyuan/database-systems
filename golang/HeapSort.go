461fbc8c923a91bbcb1fd872071673a5470d926f
tychusyuan <tychusyuan@gmail.com>
2022-06-13 20:40:28 +0800

    init

--- golang/HeapSort.go
+++ golang/HeapSort.go
@@ -5,18 +5,23 @@ import "fmt"
 func heapSort(tree []int) {
 	l := len(tree)
 	last_node := l - 1
-	parent := (last_node - 1) / 2
-	for i := parent; i > -1; i-- {
-		heap(tree, i, last_node)
-	}
 
 	for i := last_node; i > 0; i-- {
 		tree[i], tree[0] = tree[0], tree[i]
-		heap(tree, 0, i-1)
+		heapify(tree, 0, i-1)
 	}
 }
 
-func heap(tree []int, i, end int) {
+func heapBuild(tree []int) {
+	l := len(tree)
+	last_node := l - 1
+	parent := (last_node - 1) / 2
+	for i := parent; i > -1; i-- {
+		heapify(tree, i, last_node)
+	}
+}
+
+func heapify(tree []int, i, end int) {
 	l_node := 2*i + 1
 	if l_node > end {
 		return
@@ -27,15 +32,18 @@ func heap(tree []int, i, end int) {
 		max = r_node
 	}
 	if tree[i] > tree[max] {
+		// 在heap build阶段，因为是从最后一个parent向上 heapify，所以只要parent节点没有交换，就不用向下整理
+		// 在heap sort 阶段，因为已经是基于堆再调整，所以只要parent 节点没有交换，就不用向下整理
 		return
 	}
 	tree[max], tree[i] = tree[i], tree[max]
-	heap(tree, max, end)
+	heapify(tree, max, end)
 }
 
 func main() {
 	data := []int{3, 5, 3, 0, 8, 6, 1, 5, 8, 6, 2, 4, 9, 4, 7, 0, 1, 8, 9, 7, 3, 1, 2, 5, 9, 7, 4, 0, 2, 6}
 	fmt.Println(data)
+	heapBuild(data)
 	heapSort(data)
 	fmt.Println(data)
 }
